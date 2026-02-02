d47514e6fb3ec707f21f93e7c799a13ddec94626
Emin <90997900+n1k61n@users.noreply.github.com>
2026-02-02 23:37:35 +0400

    anotasiya @Column

--- Travelio/src/main/java/org/example/travelio/Entities/Place.java
+++ Travelio/src/main/java/org/example/travelio/Entities/Place.java
@@ -14,7 +14,12 @@ public class Place {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "place_name", nullable = false)
     private String placeName;
+
+    @Column(name = "city_name", nullable = false)
     private String cityName;
+
+    @Column(name = "explore_url", columnDefinition = "TEXT")
     private String exploreUrl;
 }
